aecf94a1d8656cef9042191367dabb701f369ec3
elucian9 <84775770+elucian9@users.noreply.github.com>
2021-05-25 17:24:49 +0300

    Update test_infra_poc.sh

--- tests/test_infra_poc.sh
+++ tests/test_infra_poc.sh
@@ -10,10 +10,10 @@ pip install pytest-testinfra
 
 pip install paramiko
 
-pytest -v --connection=ansible --hosts=all --ansible-inventory=../.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory test_web_connection.py
+pytest -v --connection=ansible --hosts=all --ansible-inventory=.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory test_web_connection.py
 
-pytest -v --connection=ansible --hosts=webapp --ansible-inventory=../.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory test_httpd_service.py
+pytest -v --connection=ansible --hosts=webapp --ansible-inventory=.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory test_httpd_service.py
 
-pytest -v --connection=ansible --hosts=lb --ansible-inventory=../.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory test_docker_running.py
+pytest -v --connection=ansible --hosts=lb --ansible-inventory=.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory test_docker_running.py
 
 pytest -v test_page_output.py
