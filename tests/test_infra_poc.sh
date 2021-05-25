be7c78cada009135e3dba7710081a1e0534722ca
Administrator <admin@example.com>
2021-05-25 12:07:09 +0000

    Update test_infra_poc.sh

--- tests/test_infra_poc.sh
+++ tests/test_infra_poc.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+python3 -m venv validation
+
+source validation/bin/activate
+
+pip install --upgrade pip
+
+pip install pytest-testinfra
+
+pip install paramiko
+
 pytest -v --connection=ansible --hosts=all --ansible-inventory=../.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory test_web_connection.py
 
 pytest -v --connection=ansible --hosts=webapp --ansible-inventory=../.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory test_httpd_service.py
